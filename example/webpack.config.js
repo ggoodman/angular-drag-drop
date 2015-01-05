a015c31b0048cd38585cad62d5e1cb7d320f618a
Geoff Goodman <ggoodman@gmail.com>
2015-01-05 18:52:39 -0500

    Add example

---
+++ example/webpack.config.js
@@ -0,0 +1,31 @@
+var Webpack = require("webpack");
+
+module.exports = {
+  entry: __dirname + "/src/dragular.js",
+  output: {
+    path: "./build",
+    pathInfo: false,
+    publicPath: "/build/",
+    filename: "dragular.js",
+  },
+  module: {
+    loaders: [
+      { test: /[\/\\]angular\.js$/, loader: "exports-loader?window.angular" },
+      { test: /[\/\\]angular-animate\.js$/, loader: "ng-loader?ngAnimate" },
+      { test: /\.html$/,  loader: "raw-loader" },
+      { test: /\.css$/,   loader: "style-loader!css-loader" },
+    ],
+    noParse: [
+      /[\/\\]angular\.js$/,
+      /[\/\\]angular-animate\.js$/,
+    ]
+  },
+  resolve: {
+    modulesDirectories: ["node_modules", "bower_components", "src"],
+    root: __dirname,
+    alias: {
+      'angular': "angular/angular.js",
+      'angular-animate': "angular-animate/angular-animate.js",
+    },
+  },
+};
